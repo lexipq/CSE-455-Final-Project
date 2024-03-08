70a7c6f3ab6437dcb52a14b4e314158cc2798330
Alexis Pacheco Quintano <alexipq@uw.edu>
2024-03-07 21:00:56 -0800

    trained resnet: did really bad idk why

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # testing the saved model
 import torch
+import matplotlib.pyplot as plt
 import torch.utils.data
 import torch.nn as nn
 import numpy as np
@@ -16,6 +17,17 @@ dataloader = torch.utils.data.DataLoader(data, batch_size=32, shuffle=True)
 
 predictions = []
 actual_labels = []
+
+# code for plotting visualization window (still work in progress)
+# inputs, labels = next(iter(dataloader))
+# outputs = cnn(inputs)
+# _, pred = torch.max(outputs, dim=1)
+# print("predicted label: ", pred.view(-1).cpu().numpy().tolist())
+# print("actual label: ", labels.view(-1).cpu().numpy().tolist())
+
+# img = inputs.squeeze().permute(1, 2, 0).numpy()
+# plt.imshow(img)
+# plt.show()
 
 for inputs, labels in tqdm.tqdm(dataloader):
         outputs = cnn(inputs)
