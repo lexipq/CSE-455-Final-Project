fixed major bug: forgot second batch normalization layer

model.py
@@ -109,6 +109,7 @@ class ResBlock(nn.Module):
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
         # save identity value
@@ -121,7 +122,7 @@ class ResBlock(nn.Module):
 
         # second layer
         out = self.conv2(out)
-        out = self.bn(out)
+        out = self.bn2(out)
 
         # paper starts downsampling from conv3_1
         if self.downsample is not None:

test.py
@@ -13,30 +13,30 @@ cnn = torch.load("RN18.pt")
 cnn.eval()
 
 data = loader.ImgDataSet("test.csv", transformer=model.basic_transformer)
-dataloader = torch.utils.data.DataLoader(data, batch_size=32, shuffle=True)
+dataloader = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True)
 
 predictions = []
 actual_labels = []
 
 # code for plotting visualization window (still work in progress)
-# inputs, labels = next(iter(dataloader))
-# outputs = cnn(inputs)
-# _, pred = torch.max(outputs, dim=1)
-# print("predicted label: ", pred.view(-1).cpu().numpy().tolist())
-# print("actual label: ", labels.view(-1).cpu().numpy().tolist())
+inputs, labels = next(iter(dataloader))
+outputs = cnn(inputs)
+_, pred = torch.max(outputs, dim=1)
+print("predicted label: ", pred.view(-1).cpu().numpy().tolist())
+print("actual label: ", labels.view(-1).cpu().numpy().tolist())
 
-# img = inputs.squeeze().permute(1, 2, 0).numpy()
-# plt.imshow(img)
-# plt.show()
+img = inputs.squeeze().permute(1, 2, 0).numpy()
+plt.imshow(img)
+plt.show()
 
-for inputs, labels in tqdm.tqdm(dataloader):
-        outputs = cnn(inputs)
-        _, pred = torch.max(outputs, dim=1)
-        actual_labels += labels.view(-1).cpu().numpy().tolist()
-        predictions += pred.view(-1).cpu().numpy().tolist()
+# for inputs, labels in tqdm.tqdm(dataloader):
+#         outputs = cnn(inputs)
+#         _, pred = torch.max(outputs, dim=1)
+#         actual_labels += labels.view(-1).cpu().numpy().tolist()
+#         predictions += pred.view(-1).cpu().numpy().tolist()
 
 # print("actual labels: ", actual_labels)
 # print("predicted labels: ", predictions)   
-acc = np.sum(np.array(actual_labels) == np.array(
-        predictions)) / len(actual_labels)
-print("testing accuracy:", (acc * 100))
+# acc = np.sum(np.array(actual_labels) == np.array(
+#         predictions)) / len(actual_labels)
+# print("testing accuracy:", (acc * 100))